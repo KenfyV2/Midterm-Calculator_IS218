63dbe68e940c6993bde48702f44410f59f0e0dbe
Kenfy <Andrewlmw3@gmail.com>
2024-11-08 23:56:13 -0500

    Add power plugin

---
+++ app/plugins/power.py
@@ -0,0 +1,16 @@
+"""
+This module defines the power function, which performs exponentiation of a number.
+"""
+
+def power(a, b):
+    """
+    Perform exponentiation of a number.
+
+    Args:
+        a (float): The base number.
+        b (float): The exponent.
+
+    Returns:
+        float: The result of raising `a` to the power of `b`.
+    """
+    return a ** b
